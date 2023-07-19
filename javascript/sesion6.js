76b97c648aa120d351cb2d86cec318ad9d307b29
Sharonina <shar_mdza@hotmail.com>
2023-07-18 22:08:25 -0600

    herencia

--- javascript/sesion6.js
+++ javascript/sesion6.js
@@ -11,3 +11,13 @@ const john = new Person("john", 1990, "Developer");
 john.telephone = "55555";
 console.log(john);
 console.log(john.calculateAge()); //Se debe mandar llamar la
+
+const Medico = function (name, birthYear, job) {
+  Person.call(this, name, birthYear, job); //herencia, es como 'super'
+  this.operar = () => console.log("Estoy operando");
+};
+
+const Developer = function (name, birthYear, job) {
+  Person.call(this, name, birthYear, job); //siempre lleva this
+  this.programar = () => console.log("Estoy programando");
+};
