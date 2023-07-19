efc68d10e1c9b68a33467b0eb2aa3f445c79fdde
Sharonina <shar_mdza@hotmail.com>
2023-07-18 22:09:22 -0600

    reto 2

--- javascript/sesion6.js
+++ javascript/sesion6.js
@@ -37,3 +37,25 @@ const vec2 = new Vec(2, 3);
 console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
 console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
 console.log(vec1.length()); // 2.23606797749979
+
+// Reto 2
+const Triangle = function (a, b, c) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+};
+
+Triangle.prototype.getPerimeter = function () {
+  return this.a + this.b + this.c;
+};
+
+const t1 = new Triangle(4, 6, 7);
+console.log(t1);
+console.log(t1.__proto__); //prototyoe como tal no existe, pero se puede acceder a proto para ver la funcion
+console.log(t1.getPerimeter());
+
+Object.prototype.saluda = function () {
+  console.log("hola");
+};
+
+t1.saluda(); // Imprime hola porque sigue la cadena.. t1 hereda de triangulo, que hereda de object
