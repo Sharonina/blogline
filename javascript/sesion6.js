508ce4a71261dba5869f7badfe3c9cdc2680b2b6
Sharonina <shar_mdza@hotmail.com>
2023-07-18 22:08:55 -0600

    Reto 1

--- javascript/sesion6.js
+++ javascript/sesion6.js
@@ -21,3 +21,19 @@ const Developer = function (name, birthYear, job) {
   Person.call(this, name, birthYear, job); //siempre lleva this
   this.programar = () => console.log("Estoy programando");
 };
+
+//Reto 1
+const Vec = function (x, y) {
+  this.x = x;
+  this.y = y;
+  this.plus = (vec2) => `Vec { x: ${this.x + vec2.x}, y:${this.y + vec2.y} }`;
+  this.minus = (vec2) => `Vec { x: ${this.x - vec2.x}, y:${this.y - vec2.y} }`;
+  this.length = () => Math.sqrt(this.x ** 2 + this.y ** 2);
+};
+
+const vec1 = new Vec(1, 2);
+const vec2 = new Vec(2, 3);
+
+console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
+console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
+console.log(vec1.length()); // 2.23606797749979
