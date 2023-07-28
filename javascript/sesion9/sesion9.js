7752989ecc5b6747e2a085a5361fd6b206d5bdd9
Sharonina <shar_mdza@hotmail.com>
2023-07-27 20:58:15 -0600

    usos de append child construyendo elementos

--- javascript/sesion9/sesion9.js
+++ javascript/sesion9/sesion9.js
@@ -7,3 +7,17 @@ const text = document.createTextNode("hello world");
 
 h1.appendChild(text);
 document.body.appendChild(h1);
+
+function createNode(type, child) {
+  const node = document.createElement(type);
+  const text = document.createTextNode(child);
+
+  node.appendChild(text);
+
+  return node;
+}
+
+const author = createNode("strong", "- Karl Popper");
+
+const cita = document.getElementById("quote");
+cita.appendChild(author);
