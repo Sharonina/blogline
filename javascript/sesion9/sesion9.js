a0fb242529ed3e87dceefb1eb8d46f227f021a00
Sharonina <shar_mdza@hotmail.com>
2023-07-27 20:48:23 -0600

    uso de create element, create text y append child

--- javascript/sesion9/sesion9.js
+++ javascript/sesion9/sesion9.js
@@ -1,3 +1,9 @@
 const ps = document.body.getElementsByTagName("p");
 
 document.body.insertBefore(ps[2], ps[0], ps[1]);
+
+const h1 = document.createElement("h1");
+const text = document.createTextNode("hello world");
+
+h1.appendChild(text);
+document.body.appendChild(h1);
